a2048fd11daa8a427009daff487bb26b4081074f
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-04-04 21:36:01 +0900

    add launcher examples.

---
+++ examples/launchers/mqtt_to_ros.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from argparse import ArgumentParser
+from ros_mqtt_bridge import MQTTToROS
+
+parser = ArgumentParser()
+parser.add_argument("-H", "--host", type=str, default="localhost", help="mqtt broker host")
+parser.add_argument("-P", "--port", type=int, default=1883, help="mqtt broker port")
+parser.add_argument("-FT", "--from_topic", type=str, required=True, help="from topic")
+parser.add_argument("-TT", "--to_topic", type=str, required=True, help="to topic")
+parser.add_argument("-MMN", "--message_module_name", type=str, required=True, help="message module name")
+parser.add_argument("-MCN", "--message_class_name", type=str, required=True, help="message class name")
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    mqtt_to_ros = MQTTToROS(
+        args.host, args.port, args.from_topic, args.to_topic, args.message_module_name, args.message_class_name)
+    mqtt_to_ros.start()
--- ros_mqtt_bridge/ros_to_mqtt.py
+++ ros_mqtt_bridge/ros_to_mqtt.py
@@ -8,7 +8,7 @@ import rospy
 import paho.mqtt.client as mqtt
 
 
-class ROSToMqtt(object):
+class ROSToMQTT(object):
 
     DEFAULT_NODE_NAME = "ros_to_mqtt"
 
@@ -24,7 +24,7 @@ class ROSToMqtt(object):
         self.__message_class = eval("message_module." + message_class_name)
 
         if node_name is None:
-            rospy.init_node(ROSToMqtt.DEFAULT_NODE_NAME, anonymous=True)
+            rospy.init_node(ROSToMQTT.DEFAULT_NODE_NAME, anonymous=True)
         else:
             rospy.init_node(node_name)
         self.__rospy_rate = rospy.Rate(rospy_rate)
@@ -44,20 +44,3 @@ class ROSToMqtt(object):
                 break
 
         self.__rospy_rate.sleep()
-
-
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument("-H", "--host", type=str, default="localhost", help="mqtt broker host")
-parser.add_argument("-P", "--port", type=int, default=1883, help="mqtt broker port")
-parser.add_argument("-FT", "--from_topic", type=str, required=True, help="from topic")
-parser.add_argument("-TT", "--to_topic", type=str, required=True, help="to topic")
-parser.add_argument("-MMN", "--message_module_name", type=str, required=True, help="message module name")
-parser.add_argument("-MCN", "--message_class_name", type=str, required=True, help="message class name")
-args = parser.parse_args()
-
-
-if __name__ == '__main__':
-    ros_to_mqtt = ROSToMqtt(args.host, args.port, args.from_topic, args.to_topic, args.message_module_name, args.message_class_name)
-    ros_to_mqtt.start()
