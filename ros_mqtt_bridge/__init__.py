4524f5bb4eebcee89f2172c7be3f2d64b615cf77
hiro-ya-iv <hiro-ya-iv@gmail.com>
2018-06-25 17:22:56 +0900

    add aws_iot_to_ros

--- ros_mqtt_bridge/__init__.py
+++ ros_mqtt_bridge/__init__.py
@@ -1,3 +1,4 @@
 from ros_mqtt_bridge.mqtt_to_ros import MQTTToROS
+from ros_mqtt_bridge.aws_iot_to_ros import AWSIoTToROS
 from ros_mqtt_bridge.ros_to_mqtt import ROSToMQTT
 from ros_mqtt_bridge.ros_to_aws_iot import ROSToAWSIoT
